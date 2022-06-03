ae6fb080c7be2918d8cb4d37a1c9ecae0f1b7057
HookedBehemoth <luisscheurenbrand@gmail.com>
2022-06-03 17:21:54 +0200

    add Contact and License to about

--- src/main.rs
+++ src/main.rs
@@ -847,6 +847,12 @@ fn render_about() -> rouille::Response {
             h1 { "About" }
             p { "This is a simple Pixiv API client written in Rust." }
             p { "The source code is available on " a href="https://github.com/HookedBehemoth/pixiv-proxy" { "GitHub" } "." }
+
+            h2 { "Contact" }
+            p { "If you have any questions, feel free to contact me at " a href = "mailto:info@cunnycon.org" { "info@cunnycon.org" } "." }
+
+            h2 { "License" }
+            p { "This project is licensed under the " a href="https://www.gnu.org/licenses/licenses.html#AGPL" { "GNU Affero General Public License" } "." }
         }
     };
 
